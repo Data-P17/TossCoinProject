6ebaf5bec6b31285bc4c9578846ec908a69f5432
Data-P17 <Mr.DwaynePringle@yahoo.com>
2025-02-04 03:30:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 import scipy.stats
 import streamlit as st
 import time
+import numpy as np
+import pandas as pd
 
 st.header('Tossing a Coin')
 
-chart = st.line_chart([0.5])
+chart = st.line_chart([{"Mean": 0.5}])
 
 def toss_coin(n):
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
     outcome_no = 0
     outcome_1_count = 0
+    means = []
 
     for r in trial_outcomes:
-        outcome_no +=1
+        outcome_no += 1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
+        means.append(mean)
         
-        chart.add_rows([mean])
-        
-        time.sleep(0.05)
+        # Convert to DataFrame to properly update chart
+        chart.add_rows(pd.DataFrame({"Mean": [mean]}))
+
+        time.sleep(0.05)  # Simulating real-time update
 
     return mean
 
@@ -28,4 +33,5 @@ number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
 start_button = st.button('Run')
 
 if start_button:
-    st.write(f'Running the experient of {number_of_trials} trials.')
+    st.write(f'Running the experiment with {number_of_trials} trials.')
+    toss_coin(number_of_trials)
