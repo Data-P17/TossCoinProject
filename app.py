f523bfffff689ffe46ad47236965a7c9158b4832
Data-P17 <Mr.DwaynePringle@yahoo.com>
2025-02-04 03:25:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ st.header('Tossing a Coin')
 chart = st.line_chart([0.5])
 
 def toss_coin(n):
-
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
-    mean = None
     outcome_no = 0
     outcome_1_count = 0
 
@@ -19,7 +17,9 @@ def toss_coin(n):
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
+        
         chart.add_rows([mean])
+        
         time.sleep(0.05)
 
     return mean
@@ -29,4 +29,3 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f'Running the experient of {number_of_trials} trials.')
-    mean = toss_coin(number_of_trials)
