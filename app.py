9e8bd34442aa059c80e386c39d37408f2df83673
Data-P17 <Mr.DwaynePringle@yahoo.com>
2025-02-04 01:18:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 st.header('Tossing a Coin')
 
-st.write('It is not a functional application yet. Under Construction')
+number_of _trials =st.slider('Number of trials?' ,1 ,1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+  st.write(f'Running the experiment of {number_of_trials} trails.')
+
+st.write('It is not a functional application yet. Under Construction.')
+
